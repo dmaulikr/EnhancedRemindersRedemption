9677410f63f108921d590b436d19460721936709
Bradley <bradcooper314@gmail.com>
2015-03-31 21:12:30 -0300

    Added basic notification handling in App Delegate.

--- THDEnhancedRemindersRedemption/THDEnhancedRemindersRedemption/THDAppDelegate.h
+++ THDEnhancedRemindersRedemption/THDEnhancedRemindersRedemption/THDAppDelegate.h
@@ -19,6 +19,6 @@
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
 
--(NSFetchedResultsController*) readFromTable:(NSString*)entityName
+-(NSFetchedResultsController*) readFromTable:(NSString*)entityName;
 
 @end
